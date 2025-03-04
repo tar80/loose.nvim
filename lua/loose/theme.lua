27c6115549acca52e39b918289414de2e53867a5
tar80 <tar80master@gmail.com>
2025-03-04 22:50:09 +0900

    feat(staba.nvim): adjust highlights

--- lua/loose/theme.lua
+++ lua/loose/theme.lua
@@ -784,11 +784,12 @@ function theme.highlights(colors, opts)
     if opts.plugins.staba then
       p['StabaInsertMode'] = { fg = colors.high_green, bg = colors.shade_green, style = 'bold' }
       p['StabaVisualMode'] = { fg = colors.high_purple, bg = colors.shade_gray, style = 'bold' }
+      p['StabaVisualBlockMode'] = { fg = colors.shade_gray, bg = colors.high_purple, style = 'bold' }
       p['StabaSelectMode'] = { fg = colors.bg, bg = colors.high_red, style = 'bold' }
       p['StabaReplaceMode'] = { fg = colors.high_red, bg = colors.shade_red, style = 'bold' }
       p['StabaCmdlineMode'] = { fg = colors.low_gray }
       p['StabaNC'] = { fg = colors.fg, bg = colors.nc }
-      p['StabaStatus'] = { fg = colors.fg, bg = colors.bg, sp = colors.border, style = 'underline' }
+      p['StabaStatus'] = { fg = colors.fg, sp = colors.border, style = 'underline' }
       p['StabaStatusNC'] = { fg = colors.low_gray, bg = colors.nc, sp = colors.border, style = 'underline,italic' }
       p['StabaStatusReverse'] = { fg = colors.bg, bg = colors.fg, sp = colors.fg }
       p['StabaTabFillReverse'] = { fg = colors.border, bg = colors.high_gray, sp = colors.high_gray }
